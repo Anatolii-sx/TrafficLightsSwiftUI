68c837a00b65d763dd3994217ed89bd10361fa05
Anatolii-sx <s8.8@bk.ru>
2021-10-27 00:24:47 +0300

    Initial commit

---
+++ TrafficLightsSwiftUI/ColorCircleView.swift
@@ -0,0 +1,26 @@
+//
+//  ColorCircleView.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Анатолий Миронов on 26.10.2021.
+//
+
+import SwiftUI
+
+struct ColorCircleView: View {
+    let color: Color
+    let opacity: Double
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color).opacity(opacity)
+            .frame(width: 125, height: 125)
+            .overlay(Circle().stroke(Color.white, lineWidth: 3))
+    }
+}
+
+struct ColorCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircleView(color: .red, opacity: 0.3)
+    }
+}
--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -8,9 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var titleOfButton = "START"
+    @State private var numberOfLight = 0
+    @State private var opacitiesOfLights: [String: Double] = [
+        "Red": 0.3,
+        "Yellow": 0.3,
+        "Green": 0.3
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.gray)
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                ColorCircleView(
+                    color: .red,
+                    opacity: opacitiesOfLights["Red"] ?? 0.3
+                )
+                    .padding(.top, 50)
+                ColorCircleView(
+                    color: .yellow,
+                    opacity: opacitiesOfLights["Yellow"] ?? 0.3
+                )
+                ColorCircleView(
+                    color: .green,
+                    opacity: opacitiesOfLights["Green"] ?? 0.3
+                )
+                
+                Spacer()
+                
+                Button(action: {turnLightButtonTapped()}) {
+                    Text("\(titleOfButton)")
+                        .frame(width: 125, height: 50)
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(12)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.white, lineWidth: 3)
+                        )
+                }
+                .padding(.bottom, 50)
+            }
+        }
+    }
+    
+    private func turnLightButtonTapped() {
+        changeTitleOfButton()
+        changeOpacityOfLights()
+        changeNumberOfChosenLight()
+    }
+    
+    private func changeTitleOfButton() {
+        if titleOfButton == "START" {
+            titleOfButton = "NEXT"
+        }
+    }
+    
+    private func changeOpacityOfLights() {
+        for (light, _) in opacitiesOfLights {
+            opacitiesOfLights[light] = 0.3
+        }
+        
+        switch numberOfLight {
+        case 0: opacitiesOfLights["Red"] = 1
+        case 1: opacitiesOfLights["Yellow"] = 1
+        default: opacitiesOfLights["Green"] = 1
+        }
+    }
+    
+    private func changeNumberOfChosenLight(){
+        if numberOfLight < opacitiesOfLights.count - 1 {
+            numberOfLight += 1
+        } else {
+            numberOfLight = 0
+        }
     }
 }
 
