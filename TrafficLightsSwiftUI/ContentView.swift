6a5f95eecb9c3004134a2276d7b6efe09d773dff
Anatolii-sx <s8.8@bk.ru>
2021-10-28 23:56:15 +0300

    Optimised code (version #2)

--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -7,73 +7,45 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red, yellow, green, noColor
+}
+
 struct ContentView: View {
-    @State private var titleOfButton = "START"
-    @State private var numberOfLight = 0
-    @State private var opacitiesOfLights: [String: Double] = [
-        "Red": 0.3,
-        "Yellow": 0.3,
-        "Green": 0.3
-    ]
     
+    @State private var titleOfButton = "START"
+    @State private var currentLight = CurrentLight.noColor
+
+    private func nextLight() {
+        switch currentLight {
+        case .red: currentLight = .yellow
+        case .yellow: currentLight = .green
+        default: currentLight = .red
+        }
+    }
+}
+
+extension ContentView {
     var body: some View {
         ZStack {
             Color(.gray)
                 .ignoresSafeArea()
+
             VStack(spacing: 20) {
-                ColorCircleView(
-                    color: .red,
-                    opacity: opacitiesOfLights["Red"] ?? 0.3
-                )
-                    .padding(.top, 60)
-                ColorCircleView(
-                    color: .yellow,
-                    opacity: opacitiesOfLights["Yellow"] ?? 0.3
-                )
-                ColorCircleView(
-                    color: .green,
-                    opacity: opacitiesOfLights["Green"] ?? 0.3
-                )
-                
+                ColorCircleView(color: .red, opacity: currentLight == .red ? 1 : 0.3)
+                ColorCircleView(color: .yellow, opacity: currentLight == .yellow ? 1 : 0.3)
+                ColorCircleView(color: .green, opacity: currentLight == .green ? 1 : 0.3)
+
                 Spacer()
-                
+
                 ChangeLightButton(title: titleOfButton) {
-                    turnLightButtonTapped()
+                    if titleOfButton == "START" {
+                        titleOfButton = "NEXT"
+                    }
+                    nextLight()
                 }
-                .padding(.bottom, 80)
             }
-        }
-    }
-    
-    private func turnLightButtonTapped() {
-        changeTitleOfButton()
-        changeOpacityOfLights()
-        changeNumberOfChosenLight()
-    }
-    
-    private func changeTitleOfButton() {
-        if titleOfButton == "START" {
-            titleOfButton = "NEXT"
-        }
-    }
-    
-    private func changeOpacityOfLights() {
-        for (light, _) in opacitiesOfLights {
-            opacitiesOfLights[light] = 0.3
-        }
-        
-        switch numberOfLight {
-        case 0: opacitiesOfLights["Red"] = 1
-        case 1: opacitiesOfLights["Yellow"] = 1
-        default: opacitiesOfLights["Green"] = 1
-        }
-    }
-    
-    private func changeNumberOfChosenLight(){
-        if numberOfLight < opacitiesOfLights.count - 1 {
-            numberOfLight += 1
-        } else {
-            numberOfLight = 0
+            .padding()
         }
     }
 }
